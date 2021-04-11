be8421014e0d965c7c44ce585166bd99e56294c2
Yahbs <roquesbaptiste@gmail.com>
2021-04-11 07:08:54 +0200

    Fenêtre créer

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 # -*- coding: utf-8 -*-
+
+from tkinter import *
+import socket
+import threading
+import time
+import datetime
+
+
+# Fenêtre
+
+window = Tk()
+window.title("NetWay Management")
+window.geometry(("1080x600"))
+window.minsize(1080,600)
+window.iconbitmap("./Pictures/netway.ico")
+window.config(background="#deedca")
+window.mainloop()
+
+label_title = Label(window, text="Netway Management", font=("Courrier", 40), bg="black", forground="white")
+label_title.pack(expand=yes)
+
+
+
+window.mainloop()
