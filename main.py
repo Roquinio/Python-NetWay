56a453ca504a74ed5e53d6d9a2b0abb3cff88b8d
Yahbs <roquesbaptiste@gmail.com>
2021-04-13 14:13:02 +0200

    frame actualisé

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def getWindow():
 
     # Les frames
     frameLog = Frame(window, bg="#cce6aa")
-    frameLogged = Frame(window, bg="black")
+    frameLogged = Frame(window, bg="#cce6aa")
 
     # Titre
     label_title = Label(frameLog, text="Netway Management", font=("Calibri", 40), bg="#cce6aa", fg="white")
@@ -37,12 +37,12 @@ def getWindow():
 
     # Champs login
     login_entry = Entry(frameLog, font=("Calibri", 25), bg="white", fg="#9bb07f")
-    login_entry.insert(0, "Login")
+    login_entry.insert(0, "Baptiste")
     login_entry.pack()
     
     # Champs Mot de passe
     passwd_entry = Entry(frameLog, font=("Calibri", 25), bg="white", fg="#9bb07f")
-    passwd_entry.insert(0, "Mot de passe")
+    passwd_entry.insert(0, "ESGIProjet123!")
     passwd_entry.config(show="*")
     passwd_entry.pack()
 
@@ -61,9 +61,27 @@ def getWindow():
             connexion = mysql.connector.connect(host=ip, database=db, user=login_value, password=passwd_value)
             
             if connexion.is_connected():
-                logged_title = Label(frameLogged, text="Bienvenue" + login_value + "!", font=("Calibri", 40), bg="black", fg="white")
-                logged_title.pack()
+                frameLog.pack_forget()
+                
+                logged_title = Label(frameLogged, text="Bienvenue " + login_value + " !", font=("Calibri", 40), bg="#cce6aa", fg="white")
+                logged_title.pack(expand=YES, side=TOP)
+                
+                connexion_cursor= connexion.cursor()
+                connexion_cursor.execute("SELECT * FROM *")
+                user_role = connexion_cursor.fetchall()
+                
+                logged_label = Label(frameLogged, text="Vous etes un " + user_role + " !" )
+                logged_label.pack(expand=YES)
+                
+                canvas = Canvas(frameLogged, width=width, height=height, bg="#cce6aa", bd=0, highlightthickness=0)
+                canvas.create_image(width/2, height/2, image=img)
+                canvas.pack(expand=YES, side=BOTTOM)
+                
                 frameLogged.pack(expand=YES)
+                
+                
+
+
                 
         
         except Error as e:
@@ -80,7 +98,7 @@ def getWindow():
     log_button.pack(pady=25, fill=X)
 
 
-    # Ajout de la frameLog
+    # Fermeture de frameLog
     frameLog.pack(expand=YES)
 
     # Afficher la fenêtre
