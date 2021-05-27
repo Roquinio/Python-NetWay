da8b913db620dd749d9cbae5450819ad41a1556a
Esteban <gaboriau.est@gmail.com>
2021-05-27 22:13:54 +0200

    Update conf FTP

--- Serveur/ftp.py
+++ Serveur/ftp.py
@@ -1,5 +1,5 @@
 import ftplib
-session = ftplib.FTP('server.address.com','USERNAME','PASSWORD')
+session = ftplib.FTP('test.test','test','azerty123')
 file = open('kitten.jpg','rb')                  # file to send
 session.storbinary('STOR kitten.jpg', file)     # send the file
 file.close()                                    # close file and FTP
