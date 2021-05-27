e03bd92ba8765128d7f9159fd60a1c56c37ddc29
Esteban <gaboriau.est@gmail.com>
2021-05-27 22:12:42 +0200

    Add list file ftp

---
+++ Serveur/ftplistfile.py
@@ -0,0 +1,12 @@
+files = []
+
+try:
+    files = ftp.nlst()
+except ftplib.error_perm, resp:
+    if str(resp) == "550 No files found":
+        print "No files in this directory"
+    else:
+        raise
+
+for f in files:
+    print f
