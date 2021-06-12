03eaf51681409e585e2406b3e8db54af5ad4f0c1
Asviin29 <82027817+Asviin29@users.noreply.github.com>
2021-06-12 20:50:12 +0200

    Delete port.py

--- port.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-for port in range(1, 1025):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(1)
-    result = sock.connect_ex(("#adresseip", port))
-    if 0 == result:
-        print("Port: {} Open".format(port))
-    sock.close()
