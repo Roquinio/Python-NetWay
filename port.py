b9e9ce13b272aedea3722cdb071dca21600a6252
Baptiste ROQUES <roquesbaptiste@gmail.com>
2021-06-12 22:02:21 +0200

    push var

--- port.py
+++ port.py
@@ -1,9 +1,17 @@
 import socket
 
-for port in range(1, 1025):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(1)
-    result = sock.connect_ex(("#adresseip", port))
-    if 0 == result:
-        print("Port: {} Open".format(port))
-    sock.close()
+
+
+def scan_range():
+    
+    ip = ["127.0.0.1","192.168.0.1"]
+    for i in ip :
+        for port in range(1, 1025):
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(1)
+            result = sock.connect_ex((i,port))
+            if 0 == result:
+                print("Port: {} Open".format(port))
+            sock.close()
+        
+scan_range()
