5f3ba257c23d270826df5e4189c952a6297c6e93
Yahbs <roquesbaptiste@gmail.com>
2021-07-24 21:58:54 +0200

    test

--- project/auth.py
+++ project/auth.py
@@ -5,6 +5,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required, current_user
 from .models import User
 from . import db
+import itertools
 
 auth = Blueprint('auth', __name__)
 
