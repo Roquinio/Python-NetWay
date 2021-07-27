b4653db940a0e1f2c2227e4a027c8be64f399864
Yahbs <roquesbaptiste@gmail.com>
2021-07-27 02:48:12 +0200

    Test download

--- project/auth.py
+++ project/auth.py
@@ -1,6 +1,6 @@
 # auth.py
 
-from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask import Blueprint, render_template, redirect, url_for, request, flash, send_file
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required, current_user
 from .models import User
@@ -179,3 +179,8 @@ def ftp():
 
         
     return render_template('ftp.html', file=file)
+
+@auth.route('/srv/Python-NetWay/project/ftp/<filename>')
+@login_required
+def files_download(filename):
+    return send_file(filename, as_attachment=True)
