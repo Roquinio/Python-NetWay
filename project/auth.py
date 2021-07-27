7ed7dccbce8044c0279f5b65b314be88e66b0123
Yahbs <roquesbaptiste@gmail.com>
2021-07-27 02:51:26 +0200

    test fix

--- project/auth.py
+++ project/auth.py
@@ -183,4 +183,4 @@ def ftp():
 @auth.route('/srv/Python-NetWay/project/ftp/<filename>')
 @login_required
 def files_download(filename):
-    return send_file(filename, as_attachment=True)
+    return send_file(filename)
